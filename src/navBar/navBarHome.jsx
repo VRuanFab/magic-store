10f3ea236ddadcf439a5c65d6cb941a982a2900e
VRuanFab <victor.ruanfab@gmail.com>
2024-02-11 03:53:04 -0300

    comecando com navbar

--- src/navBar/navBarHome.jsx
+++ src/navBar/navBarHome.jsx
@@ -1,8 +1,46 @@
+import {
+    BiSearch,
+    IoIosColorWand,
+    IoMdRose
+} from '../svgs/index'
+
 
 function HomeBar() {
     return (
-        <div className="w-full p-2 px-5 bg-purple-500">
-            aa
+        <div className="w-full py-[5px] px-5 bg-purple-500">
+            <div className="grid grid-cols-3">
+
+                <div className="grid w-fit">
+                    <div className='justify-self-center'>
+                        <IoMdRose/>
+                    </div>
+                    <label for="search" className=''>Homepage</label>
+                </div>
+
+                <div className="flex place-self-center">
+                    <input type="text" id="search" className='rounded-lg outline-none px-[10px]
+                    focus:outline-4 focus:outline-offset-4'/>
+                    <label for="search" className='border-2 p-[5px]
+                    rounded-full ml-2'><BiSearch/></label>
+                </div>
+                
+                <div className="flex place-self-center">
+                    <div className="grid w-fit">
+                        <div className='justify-self-center'>
+                            <IoMdRose/>
+                        </div>
+                        <label for="search" className=''>Homepage</label>
+                    </div>
+
+                    <div className="grid w-fit">
+                        <div className='justify-self-center'>
+                            <IoMdRose/>
+                        </div>
+                        <label for="search" className=''>Homepage</label>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 }
---
+++ src/svgs/index.jsx
@@ -0,0 +1,10 @@
+import { BiSearch } from "react-icons/bi";
+import { IoIosColorWand } from "react-icons/io";
+import { IoMdRose } from "react-icons/io";
+
+
+export {
+    BiSearch,
+    IoIosColorWand,
+    IoMdRose
+}
