af45329332167ee8c6a487ee93651e79f5b09105
VRuanFab <victor.ruanfab@gmail.com>
2024-02-13 02:15:44 -0300

    svgs e estilizando

--- src/svgs/index.jsx
+++ src/svgs/index.jsx
@@ -1,10 +1,25 @@
 import { BiSearch } from "react-icons/bi";
 import { IoIosColorWand } from "react-icons/io";
 import { IoMdRose } from "react-icons/io";
+import { FaFrog } from "react-icons/fa";
+import { MdOutlineMenuBook } from "react-icons/md";
+import { GiSpottedMushroom } from "react-icons/gi";
+import { GiAcid } from "react-icons/gi";
+import { GiMagicBroom } from "react-icons/gi";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { RiAccountCircleFill } from "react-icons/ri";
 
 
 export {
     BiSearch,
     IoIosColorWand,
-    IoMdRose
+    IoMdRose,
+    FaFrog,
+    MdOutlineMenuBook,
+    GiSpottedMushroom,
+    GiAcid,
+    GiMagicBroom,
+
+    RiAccountCircleLine,
+    RiAccountCircleFill
 }
